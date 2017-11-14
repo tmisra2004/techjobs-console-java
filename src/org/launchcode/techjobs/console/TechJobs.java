a717f4a4b2e2e69d00548c26132f04e8899a1af8
Tavish Misra <tmisra2004@gmail.com>
2017-11-13 20:09:31 -0600

    Implemented printJobs function

--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -2,6 +2,7 @@ package org.launchcode.techjobs.console;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -111,6 +112,21 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
-        System.out.println("printJobs is not implemented yet");
+        // if jobs is empty or unavailable print line error statement
+        if (someJobs.isEmpty()) {
+            System.out.println("");
+            System.out.println("There are no jobs that match your search. Please try another query.");
+        }
+
+        // add nested for loop to print out each item in correct formatting
+        for(int i = 0; i < someJobs.size(); i++) {
+            System.out.println("\n*****");
+
+            for(Map.Entry<String, String> job : someJobs.get(i).entrySet() ) {
+                System.out.println(job.getKey() + ": " + job.getValue());
+            }
+            System.out.println("*****");
+
+        }
     }
 }
